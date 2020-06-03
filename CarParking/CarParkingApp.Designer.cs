fc851326645df8bc08bb8a0c28cffac33a612ea1
vrushali ahire <ahire.vrushali@gmail.com>
2020-06-03 23:31:55 +0530

    First draft without db opration

---
+++ CarParking/CarParkingApp.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace CarParking
+{
+    partial class CarParkingApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Panel1 = new System.Windows.Forms.Panel();
+            this.btn_Exist = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.lblLoding = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Panel1
+            // 
+            this.Panel1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Panel1.Location = new System.Drawing.Point(117, 24);
+            this.Panel1.Name = "Panel1";
+            this.Panel1.Size = new System.Drawing.Size(365, 261);
+            this.Panel1.TabIndex = 0;
+            // 
+            // btn_Exist
+            // 
+            this.btn_Exist.Location = new System.Drawing.Point(251, 375);
+            this.btn_Exist.Name = "btn_Exist";
+            this.btn_Exist.Size = new System.Drawing.Size(75, 23);
+            this.btn_Exist.TabIndex = 1;
+            this.btn_Exist.Text = "Exit";
+            this.btn_Exist.UseVisualStyleBackColor = true;
+            this.btn_Exist.Click += new System.EventHandler(this.btn_Exist_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(117, 341);
+            this.progressBar1.Maximum = 40;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(365, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 2;
+            // 
+            // lblLoding
+            // 
+            this.lblLoding.AutoSize = true;
+            this.lblLoding.Location = new System.Drawing.Point(256, 304);
+            this.lblLoding.Name = "lblLoding";
+            this.lblLoding.Size = new System.Drawing.Size(70, 13);
+            this.lblLoding.TabIndex = 3;
+            this.lblLoding.Text = "Refreshing....";
+            this.lblLoding.Visible = false;
+            // 
+            // CarParkingApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(614, 410);
+            this.Controls.Add(this.lblLoding);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btn_Exist);
+            this.Controls.Add(this.Panel1);
+            this.Name = "CarParkingApp";
+            this.Text = "CarParkingApp";
+            this.Load += new System.EventHandler(this.CarParkingApp_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Panel1;
+        private System.Windows.Forms.Button btn_Exist;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label lblLoding;
+    }
+}
---
+++ CarParking/CarParkingApp.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarParking
+{
+    public partial class CarParkingApp : Form
+    {
+        public CarParkingApp()
+        {
+            InitializeComponent();
+        }
+        private System.Windows.Forms.Timer timer1;
+        public void InitTimer()
+        {
+            timer1 = new System.Windows.Forms.Timer();
+            timer1.Tick += new EventHandler(renderPanel);
+            timer1.Interval = 10000;
+            timer1.Start(); 
+        }
+        
+        private void CarParkingApp_Load(object sender, EventArgs e)
+        {
+          renderPanel(sender, e);
+            InitTimer();
+            
+        }
+        
+        private void renderPanel(object sender, EventArgs e)
+        {
+            lblLoding.Visible = true;
+            progressBar1.Maximum = 40;
+            progressBar1.Value = 0;
+            
+            int[] al = new int[20];
+            int genRand;
+            Random r;
+            for (int i = 0; i < 20; i++)
+            {
+                r = new Random();
+                genRand = r.Next(0, 3);
+                Task.Delay(200).Wait();
+                al[i] = genRand;
+                progressBar1.Value++;
+            }
+            int rowcounter = 5;
+            int columncounter = 4;
+            // int totalcount = 18;
+            int elementcout = 0;
+            Panel1.Controls.Clear();
+            for (int i = 0; i < rowcounter; i++)
+            {
+                for (int j = 0; j < columncounter; j++)
+                {
+                    progressBar1.Value++;
+                    Label lable = new Label();
+                    Panel1.Controls.Add(lable);
+                    lable.Top = (i * 50);
+                    lable.Width = 80;
+                    lable.Left = j * 90;
+                    lable.BackColor = Color.Aqua;
+                    lable.TextAlign = ContentAlignment.MiddleCenter;
+                    lable.Text = "P" + elementcout;
+
+                    if (al[elementcout] == 0)
+                    {
+                        lable.BackColor = Color.Green;
+                    }
+                    else if (al[elementcout] == 1)
+                    {
+                        lable.BackColor = Color.Yellow;
+                    }
+                    if (al[elementcout] == 2)
+                    {
+                        lable.BackColor = Color.Red;
+                    }
+                    elementcout++;
+                    //if (elementcout >= totalcount)
+                    //{
+                    //    break;
+                    //}
+                }
+               
+            }
+            lblLoding.Visible = false;
+        }
+        
+      
+        private void btn_Exist_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+       
+    }
+}
